b96ab600374e4683f6c3757a23a88710831c4082
Zane <zeisen33@gmail.com>
2022-12-01 15:22:03 -0500

    two-sum

--- lib/TDD.rb
+++ lib/TDD.rb
@@ -1,11 +1,25 @@
 def my_uniq(arr)
     result = []
     arr.each_with_index do |ele, i|
-        if i == arr.length-1
+        if i == arr.length
             next 
         else  
-            result << ele if !arr[i+1..-1].include?(ele)
+            result << ele if !arr[i + 1..-1].include?(ele)
         end
     end
-result
+    result
 end
+# p my_uniq([1,2,2,3]) # => [1, 2, 3]
+
+class Array
+    def two_sum
+        arr = []
+        (0...self.length).each do |idx|
+            (idx + 1...self.length).each do |jdx|
+                arr << [idx, jdx] if self[idx] + self[jdx] == 0 && !arr.include?([jdx, idx])
+            end
+        end
+        return arr
+    end
+end
+
--- spec/TDD_spec.rb
+++ spec/TDD_spec.rb
@@ -4,19 +4,37 @@ describe "my_uniq" do
     let(:array) { [1, 2, 2, 4]}
     context "when array has duplicates" do 
         it "removes duplicates" do
-            expect(array.uniq).to eq([1, 2, 4])
+            expect(my_uniq(array)).to eq([1, 2, 4])
         end 
     end
 
     context "when array has no duplicates" do 
         it "returns the array" do 
-            expect([1, 2, 3].uniq).to eq([1, 2, 3])
+            expect(my_uniq(array)).to eq([1, 2, 4])
         end
     end
 
     it "should not modify the original array" do 
         id = array.object_id
-        array.uniq
+        my_uniq(array)
         expect(id).to be(array.object_id)
     end
+end
+
+describe "two_sum" do
+    let (:arr) {[-1,0,2,-2,1]}
+    context "when the array has pairs that sum to zero" do
+        it "returns an array with the indices of the pairs" do
+            expect(arr.two_sum).to eq([[0,4], [2,3]])
+        end
+        it "should sort the returned array from smallest to largest index" do
+            expect([-1,1,2,-2,1].two_sum).to eq([[0,1],[0,4],[2,3]])
+        end
+    end
+    context "when the array has no pairs that sum to zero" do
+        it "should return an empty array" do
+            expect([4,3,2].two_sum).to eq([])
+        end
+    end
+    
 end
